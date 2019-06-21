6001644d679c0765474c01a76634dd9c7165cf74
Lexi127 <aperalta127@gmail.com>
2019-06-21 16:49:38 -0700

    created get routes for scraping

--- server.js
+++ server.js
@@ -3,14 +3,14 @@ const handlebars = require("express-handlebars");
 const mongoose = require("mongoose");
 const axios = require("axios");
 const cheerio = require("cheerio");
-const db = require("./models");
+const db = require("./models/models.js");
 
 const PORT = process.env.PORT || 3000;
 const MONGO_DB = process.env.MONGODB_URI || "mongodb://localhost/newsDB";
 
 mongoose.connect(MONGO_DB);
 mongoose.set("useFindAndModify", false);
-const models = require("./models");
+const models = require("./models/models");
 models.NewsPost.createCollection();
 
 const app = express();
@@ -23,12 +23,52 @@ app.use(express.static("public"));
 
 //Routes
 
+//GET route for scraping the site
+
 app.get("/scrape", (req, res) => {
-  axios.get("https://old.reddit.com/r/science/").then(res => {
-    const $ = cheerios.load(response.data);
+  axios.get("https://old.reddit.com/r/science/").then(response => {
+    const $ = cheerio.load(response.data);
+
+    $("div.top-matter").each(function(i, element) {
+      const result = {};
+
+      result.title = $(this)
+        .children("p.title")
+        .text();
+      result.link = $(this)
+        .children("p.title")
+        .children("a")
+        .attr("href");
+
+      db.NewsPost.create(result)
+        .then(function(dbNewsPost) {
+          console.log(dbNewsPost);
+        })
+        .catch(function(err) {
+          console.log(err);
+        });
+    });
+    res.send("Scrape Complete");
   });
 });
 
-app.post("/api/comment", (req, res) => {});
+//Route for getting all Articles from the db
+
+app.get("/stories", (req, res) => {
+  db.NewsPost.find({})
+    .then(dbNewsPost => {
+      res.json(dbNewsPost);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
+app.post("/api/comment", (req, res) => {
+  // const {id, name, message} = req.body;
+  // if(!id) {
+  //     response.send()
+  // }
+});
 
 app.listen(PORT, () => console.log("App running on port" + PORT));
